0c35a4f590d951da87f47e076aa39c954421b4a6
xtrangek <bryan.alcudia@outlook.com>
2024-12-01 01:36:33 -0500

    primer commit

---
+++ src/main/java/com/example/HelloServlet.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        response.getWriter().println("Hola, este es un servlet funcionando correctamente.");
+    }
+}
