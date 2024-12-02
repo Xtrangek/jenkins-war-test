4a6960baf791e317e7df077ee1ca78cd342d4bd6
xtrangek <bryan.alcudia@outlook.com>
2024-12-02 00:29:55 -0500

    correccion class

--- src/main/java/com/example/HelloServlet.java
+++ src/main/java/com/example/HelloServlet.java
@@ -1,15 +1,20 @@
 package com.example;
 
 import java.io.IOException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
+//@WebServlet("/hello")
 public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws IOException {
         response.getWriter().println("Hola, este es un servlet funcionando correctamente.");
+        String appName = getServletContext().getInitParameter("appName");
+        response.getWriter().println("Nombre de la aplicación: " + appName);
     }
 }
+
+
